ad946d8bee78653eb6dbdcc56ba976825e26953f
abduadem <abdulrehmanadem4@gmail.com>
2023-04-27 21:09:58 -0700

    do while 001

---
+++ do_while-001.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int n=1;
+//printing 1-9 using do while loop
+
+    do
+    {
+        cout<<n;
+        n++;
+    } while (n<10);
+
+    return 0;
+}
