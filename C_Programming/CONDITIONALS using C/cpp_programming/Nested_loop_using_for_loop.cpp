ef1e0a4bafe30983812ea28e84539b8405c4c176
abduadem <abdulrehmanadem4@gmail.com>
2023-04-27 20:39:19 -0700

    Nested_loop_using_for_loop.cpp

---
+++ C_Programming/CONDITIONALS using C/cpp_programming/Nested_loop_using_for_loop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+
+    int n=1;
+    int m=1;
+
+    //Nested loops using For loops
+    cout<<"Nested loops using for loops"<<"\n";
+
+    for(n=1;n<10;n++){
+        for(m=1;m<10;m++){
+            cout<<"#";
+        }
+        cout<<"\n";
+    }
+    cout<<"\n\n";
+}
